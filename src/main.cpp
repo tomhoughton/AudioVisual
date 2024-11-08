19328b22fd29e290f261bea21099354a6b23680e
Tom Houghton <tom@smithmyers.com>
2024-11-08 17:36:15 +0000

    Struct for WavHeader

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,22 @@
 
 int main() {
     std::cout << "Hello World" << std::endl;
+
+    struct WavHeader {
+        std::string RIFF; // 1-4
+        int fileSize; // 5-8
+        std::string fileType; // 9-12
+        std::string formatChunkMarker; // 13-16
+        int formatLength; // 17-20
+        int formatType; // 21-22
+        int noChannels; // 23-24
+        int sampleRate; // 25-28
+        int byteRate; // 29-32
+        int blockAlign; // 33-34
+        int bitsPerSample; // 35-36
+        int subChunk2ID; // 37-40
+        int subChunk2Size; // 41-44
+    }
+
     return 0;
 }
